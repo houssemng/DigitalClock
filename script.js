575321d3c1e51ad874663f087d8b3f115772c194
Xavi <houssemnaguez@gmail.com>
2023-04-16 14:14:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minutes");
+const second = document.getElementById("seconds");
+const ampm = document.getElementById("am-pm");
+
+
+function updateClcok(){
+	let h = new Date().getHours();
+	let m = new Date().getMinutes();
+	let s = new Date().getSeconds();
+	let ampm = "AM";
+
+
+	if(h > 12){
+		h = h - 12
+		ampm = "PM"
+	}
+
+
+	h = h < 10 ? "0" + h : h;
+	m = m < 10 ? "0" + m : m;
+	s = s < 10 ? "0" + s : s;
+
+	hour.innerText = h;
+	minute.innerText = m;
+	second.innerText = s;
+	ampm, (innerText = ampm);
+	setTimeout(()=>{
+		updateClcok()
+	}, 1000)
+}
+
+updateClcok()
